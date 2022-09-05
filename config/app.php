4af84ef791aee149c9eba3cbec0832a981c99d83
MikeHermsen <mikehermsenprive@gmail.com>
2022-09-05 13:43:05 +0200

    Added QR-Code

--- config/app.php
+++ config/app.php
@@ -159,6 +159,8 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+
+        Milon\Barcode\BarcodeServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -210,6 +212,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-    ])->toArray(),
+        'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
+        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
+        ])->toArray(),
+
 
 ];
